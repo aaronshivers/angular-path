b891d05c3d1aa628dff0afc2e2e8cbc0cdc056bf
aaron shivers <aaorn@moonmail.org>
2019-11-07 12:34:26 -0600

    week1-day3 added bigDiff and fizzArray

---
+++ fizzArray.js
@@ -0,0 +1,13 @@
+var fizzArray = function (n) {
+    if (n === 0)
+        return [];
+    var array = [];
+    for (var i = 0; i < n; i++) {
+        array.push(String(i));
+    }
+    return array;
+};
+console.log(fizzArray(4)); // ["0", "1", "2", "3"]
+console.log(fizzArray(10)); // ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+console.log(fizzArray(2)); // ["0", "1"]
+console.log(fizzArray(0)); // []
---
+++ fizzArray.ts
@@ -0,0 +1,16 @@
+const fizzArray = n => {
+  if (n === 0) return []
+
+  const array = []
+
+  for (let i = 0; i < n; i++) {
+    array.push(String(i))
+  }
+
+  return array
+}
+
+console.log(fizzArray(4)) // ["0", "1", "2", "3"]
+console.log(fizzArray(10)) // ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+console.log(fizzArray(2)) // ["0", "1"]
+console.log(fizzArray(0)) // []
