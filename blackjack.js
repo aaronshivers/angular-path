f476ed397d2fd9be3cb15319643c11a5ba44d242
aaron shivers <aaorn@moonmail.org>
2019-11-07 12:40:10 -0600

    week1-day4 added blackjack

---
+++ blackjack.js
@@ -0,0 +1,17 @@
+var Blackjack = /** @class */ (function () {
+    function Blackjack() {
+    }
+    Blackjack.hasBusted = function (val) {
+        return val > 21;
+    };
+    Blackjack.getHighCard = function (val1, val2) {
+        if (this.hasBusted(val1) && this.hasBusted(val2))
+            return 0;
+        return Math.max(val1, val2);
+    };
+    return Blackjack;
+}());
+console.log(Blackjack.getHighCard(19, 21)); // 21
+console.log(Blackjack.getHighCard(21, 19)); // 21
+console.log(Blackjack.getHighCard(19, 22)); // 19
+console.log(Blackjack.getHighCard(23, 22)); // 0
